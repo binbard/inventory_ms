1de294cd5b112b602a2661f27e13a0ade42da64e
binbard <harshitjawla123@gmail.com>
2025-01-17 22:06:43 +0530

    Init commit 2

--- inventory_system/settings.py
+++ inventory_system/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-s%8n74%#7u#gw_(e5tx-hwn9%@pt4p7kkncl$6z7h14mmiz2mq
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
